ecce45c971ddd78a7441e98c2a7a10c664f58ed1
Neev Singh Lamba <52173761+neev-lamba@users.noreply.github.com>
2024-06-11 14:38:18 -0400

    Update DBInitializer_Transaction.java

--- develop/credit/credit/src/main/java/com/example/credit/config/DBInitializer_Transaction.java
+++ develop/credit/credit/src/main/java/com/example/credit/config/DBInitializer_Transaction.java
@@ -14,20 +14,13 @@ public class DBInitializer_Transaction {
     @Bean
     CommandLineRunner initDatabase(TransactionRepository transactionRepository) {
         return args -> {
-            var transactions = List.of(
-                    new Transaction(1, 1, "2024-06-01 10:00:00", 150.00, "Walmart", "Walmart", "Completed", "USD"),
-                    new Transaction(2, 2, "2024-06-02 12:30:00", 250.00, "Amazon", "Amazon", "Completed", "EUR"),
-                    new Transaction(3, 3, "2024-06-03 14:45:00", 50.00, "Starbucks", "Starbucks", "Completed", "USD"),
-                    new Transaction(4, 4, "2024-06-04 09:15:00", 300.00, "Best Buy", "Best Buy", "Completed", "USD"),
-                    new Transaction(5, 5, "2024-06-05 11:00:00", 75.00, "Target", "Target", "Completed", "USD"),
-                    new Transaction(6, 6, "2024-06-06 16:30:00", 200.00, "Barnes & Noble", "Barnes & Noble",
-                            "Completed", "USD"),
-                    new Transaction(7, 7, "2024-06-07 13:45:00", 400.00, "Apple Store", "Apple Store", "Completed",
-                            "USD"),
-                    new Transaction(8, 8, "2024-06-08 17:00:00", 100.00, "Costco", "Costco", "Completed", "USD"),
-                    new Transaction(9, 9, "2024-06-09 15:15:00", 50.00, "Uber", "Uber", "Completed", "EUR"),
-                    new Transaction(10, 10, "2024-06-10 18:30:00", 250.00, "Home Depot", "Home Depot", "Completed",
-                            "USD"));
+            List<Transaction> transactions = List.of(
+                    new Transaction("John Dole", "1111222233334444", "1", "2024-06-01 10:00:00", 150.00, "Walmart", "Walmart", "Completed", "USD"),
+                    new Transaction("Ronald Jones", "2222333344445555", "2", "2024-06-02 12:30:00", 250.00, "Amazon", "Amazon", "Completed", "EUR"),
+                    new Transaction("Daisy Bush", "3333444455556666", "3", "2024-06-03 14:45:00", 50.00, "Starbucks", "Starbucks", "Completed", "USD"),
+                    new Transaction("Rose Waters", "4444555566667777", "4", "2024-06-04 09:15:00", 300.00, "Best Buy", "Best Buy", "Completed", "USD"),
+                    new Transaction("Penelope Downs", ""5555666677778888", "5", "2024-06-05 11:00:00", 75.00, "Target", "Target", "Completed", "USD"),
+                   );
             
             transactionRepository.saveAll(transactions);
         };
