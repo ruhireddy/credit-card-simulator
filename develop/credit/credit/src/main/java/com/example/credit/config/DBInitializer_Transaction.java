b9a67af86d55a63e7f2a2fb957d7ec2fd8292b12
Neev Singh Lamba <52173761+neev-lamba@users.noreply.github.com>
2024-06-11 14:42:41 -0400

    Update DBInitializer_Transaction.java

--- develop/credit/credit/src/main/java/com/example/credit/config/DBInitializer_Transaction.java
+++ develop/credit/credit/src/main/java/com/example/credit/config/DBInitializer_Transaction.java
@@ -19,7 +19,7 @@ public class DBInitializer_Transaction {
                     new Transaction("Ronald Jones", "2222333344445555", "2", "2024-06-02 12:30:00", 250.00, "Amazon", "Amazon", "Completed", "EUR"),
                     new Transaction("Daisy Bush", "3333444455556666", "3", "2024-06-03 14:45:00", 50.00, "Starbucks", "Starbucks", "Completed", "USD"),
                     new Transaction("Rose Waters", "4444555566667777", "4", "2024-06-04 09:15:00", 300.00, "Best Buy", "Best Buy", "Completed", "USD"),
-                    new Transaction("Penelope Downs", ""5555666677778888", "5", "2024-06-05 11:00:00", 75.00, "Target", "Target", "Completed", "USD"),
+                    new Transaction("Penelope Downs", "5555666677778888", "5", "2024-06-05 11:00:00", 75.00, "Target", "Target", "Completed", "USD"),
                    );
             
             transactionRepository.saveAll(transactions);
