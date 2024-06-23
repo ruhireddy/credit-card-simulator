2c308760c49b54ea00b733665c78b3fc8086608f
johngarzon-ferrer <jgarzonferrer@gmail.com>
2024-06-23 13:34:38 -0400

    [POC] username/login redirect submitting username and password will redirect to specified page with custom url... next steps are to add additional js and css to style data from given json

--- develop/credit/credit/src/main/resources/static/main.js
+++ develop/credit/credit/src/main/resources/static/main.js
@@ -1,2 +1,19 @@
 console.log("hello world");
 
+window.addEventListener('load', () => {
+    const form = document.querySelector("#login-form");
+    const username = document.querySelector("#username");
+    const password = document.querySelector("#password");
+
+	form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        console.log(username.value);
+        console.log(password.value);
+        
+        var new_url = "http://localhost:8080/api/card/" + username.value;
+        window.location.replace(new_url);
+
+	});
+});
+
